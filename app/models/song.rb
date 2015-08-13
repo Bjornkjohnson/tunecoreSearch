6754d3c1c7db2fe56a98eda19b6202e57f2b9e7f
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-08-13 11:06:09 -0400

    add Readme

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,10 +3,15 @@ class Song < ActiveRecord::Base
 
 	def self.text_search(query)
 		if query.present?
-			query = query.split(" ").join(" | ")
+			rank = <<-RANK
+				ts_rank(to_tsvector(title), plainto_tsquery(#{sanitize(query)})) +
+				ts_rank(to_tsvector(album), plainto_tsquery(#{sanitize(query)})) +
+				ts_rank(to_tsvector(artist), plainto_tsquery(#{sanitize(query)}))
+			RANK
+			#query = query.split(" ").join(" | ")
 			#to_tsquery(temp)query
-			where("title @@ to_tsquery(:q) or album @@ to_tsquery(:q) or artist @@ to_tsquery(:q)", q: query)
-			#where(":q @@ title or :q @@ album or :q @@ artist ", q: query)
+			#where("title @@ to_tsquery(:q) or album @@ to_tsquery(:q) or artist @@ to_tsquery(:q)", q: query)
+			where(":q @@ title or :q @@ album or :q @@ artist ", q: query).order("#{rank} desc")
 		else
 			all
 		end
