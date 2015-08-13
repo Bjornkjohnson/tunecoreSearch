7cb6cfb72de59e4162c4c9ca4b71ff5ccf03e587
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-08-13 19:42:14 -0400

    attempt to improve search results ranking

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,15 +3,20 @@ class Song < ActiveRecord::Base
 
 	def self.text_search(query)
 		if query.present?
-			rank = <<-RANK
-				ts_rank(to_tsvector(title), plainto_tsquery(#{sanitize(query)})) +
-				ts_rank(to_tsvector(album), plainto_tsquery(#{sanitize(query)})) +
-				ts_rank(to_tsvector(artist), plainto_tsquery(#{sanitize(query)}))
-			RANK
 			query = query.split(" ").join(" | ")
+			rank = <<-RANK
+				ts_rank(to_tsvector(title), to_tsquery(#{sanitize(query)})) +
+				ts_rank(to_tsvector(album), to_tsquery(#{sanitize(query)})) +
+				ts_rank(to_tsvector(artist), to_tsquery(#{sanitize(query)}))
+			RANK
+			k
+			
 			#to_tsquery(temp)query
-			where("title @@ to_tsquery(:q) or album @@ to_tsquery(:q) or artist @@ to_tsquery(:q)", q: query).order("#{rank} desc")
-			#where(":q @@ title or :q @@ album or :q @@ artist ", q: query).order("#{rank} desc")
+			where("title @@ to_tsquery(:q) or album @@ to_tsquery(:q) or artist @@ to_tsquery(:q)", q: query).order("#{rank} DESC")
+			#where(":q @@ title or :q @@ album or :q @@ artist ", q: query).order("#{rank} DESC")
+
+
+
 		else
 			all
 		end
