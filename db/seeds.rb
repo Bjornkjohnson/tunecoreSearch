aa5f17b43d542d6b9003cd6cae295c325c9bb233
Bjorn Johnson <bjornkjohnson@gmail.com>
2015-08-13 20:53:25 -0400

    fix absolute path for logo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-# Song.create!(title:  	"Sympathy for the Devil",
-#              album: 	"Beggars Banquet",
-#              artist: 	"The Rolling Stones")
-
-# Song.create!(title:  	"Rip This Joint",
-#              album: 	"Exile on Main Street",
-#              artist: 	"The Rolling Stones")
-
-# 499.times do |n|
-#   title  = "title-#{n+1}"
-#   album = "album-#{n+1}"
-#   artist = "artist-#{n+1}"
-#   Song.create!(title:  title,
-#                album: album,
-#                artist: artist)
-# end
-
-
 require "net/http"
 require 'json'
 require "uri"
@@ -34,10 +9,7 @@ def callApi(thing)
 end
 
 def get_track_names(j)
-	
 	totalTracks =  j["tracks"]["total"].to_i
-	
-
 	for i in 0...totalTracks
 		Song.create!(title:  	j["tracks"]["items"][i]["name"],
              album: 	j["name"],
